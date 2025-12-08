f70ca78ce8bbf978e94016b093242a4dc1dfeb35
Louis Maher <louismaher@gmail.com>
2025-12-08 12:06:25 -0500

    Themes picker out

--- _source/assets/js/app.js
+++ _source/assets/js/app.js
@@ -5,4 +5,4 @@ import animateOnScroll from './_elements/animate-on-scroll.js';
 import PopoverMenu from './_elements/popover-menu.js';
 import RandomShapes from './_elements/random-shapes.js';
 import SmallDetails from './_elements/small-details.js';
-import ThemePicker from './_elements/theme-picker.js';
+/*import ThemePicker from './_elements/theme-picker.js';*/
